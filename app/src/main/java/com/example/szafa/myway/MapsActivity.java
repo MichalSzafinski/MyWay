30fdf1e558fef11ee254a84b51caf8a72c3db40a
Michał Szafiński <michal.szafinski@gmail.com>
2019-01-09 15:01:35 +0100

    Setting and displaying shortest route works

--- app/src/main/java/com/example/szafa/myway/MapsActivity.java
+++ app/src/main/java/com/example/szafa/myway/MapsActivity.java
@@ -4,7 +4,9 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
@@ -16,6 +18,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.Pair;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -27,9 +31,25 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.DistanceMatrixApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.DirectionsStep;
+import com.google.maps.model.DistanceMatrix;
+import com.google.maps.model.Duration;
+import com.google.maps.model.EncodedPolyline;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -38,19 +58,38 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private FusedLocationProviderClient mFusedLocationClient;
     private LocationManager locationManager;
     private DrawerLayout mDrawerLayout;
+    private LatLng ClientLocation = new LatLng(40,40);
+
+    public String[] RouteAddresses = new String[] {"Aleje Jerozolimskie", "Obozowa Warszawa", "Koszykowa Warszawa", "Stadion Narodowy"};
+    public String StartAddress = "Miejski Ogrod zoologiczny warszawa"; // if StartAddress is null then StartAdress is the current client location
+    public String EndAddress = null; // if EndAddress is null then EndAddress is the current client location
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        setNavBar();
+         setNavBar();
+    }
+    private void SwitchToRouteLocationsActivity()
+    {
+        Intent intent = new Intent(this, RouteLocationsActivity.class);
+        startActivity(intent);
+    }
+    private void SwitchToOptions()
+    {
+        Intent intent = new Intent(this, OptionsActivity.class);
+        startActivity(intent);
+    }
+    private void a(String aa)
+    {
+        Toast.makeText(this, aa, Toast.LENGTH_LONG).show();
     }
     private void setNavBar()
     {
@@ -70,6 +109,19 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                         // Add code here to update the UI based on the item selected
                         // For example, swap UI fragments here
 
+                        a(menuItem.getTitle().toString());
+                        // MENU ITEMS CLICKS
+
+                        if(menuItem.getTitle().toString().contains("Client list"))
+                        {
+                            SwitchToRouteLocationsActivity();
+                        }
+                        if(menuItem.getTitle().toString().contains("Options"))
+                        {
+                            SwitchToOptions();
+                        }
+
+
                         return true;
                     }
                 });
@@ -80,13 +132,20 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
         setTitle("MyWay");
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+
             case android.R.id.home:
                 mDrawerLayout.openDrawer(GravityCompat.START);
+
                 return true;
+
+
         }
+        Intent intent = new Intent(this, RouteLocationsActivity.class);
+        startActivity(intent);
         return super.onOptionsItemSelected(item);
     }
 
@@ -112,7 +171,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     public void onSuccess(Location location) {
                         // Got last known location. In some rare situations this can be null.
                         if (location != null) {
-
+                            ClientLocation = new LatLng(location.getLatitude(), location.getLongitude());
                             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 10));
                         }
                     }
@@ -124,28 +183,151 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     public void onClickBtn(View v) //Set route button click
     {
-        //Toast.makeText(this, "Clicked on Button", Toast.LENGTH_LONG).show();
+        DisplayShortestRoute();
 
-        Intent intent = new Intent(this, RouteLocationsActivity.class);
-        startActivity(intent);
+        //Intent intent = new Intent(this, RouteLocationsActivity.class);
+        //startActivity(intent);
+    }
+
+    public void DisplayShortestRoute()
+    {
+        GeoApiContext context = new GeoApiContext.Builder().apiKey("AIzaSyBsuJVWuZodkTy2qHjMqaEtYCDy_4hq7Yo").build();
+
+        List<LatLng> path = new ArrayList();
+        List<Pair<LatLng, String>> markers = new ArrayList<>();
+
+        long JourneyTime = 0; //in sec
+        int JourneyLength = 0; //in meters
+
+        try {
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                ClientLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                                mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+                            }
+                        }
+                    });
 
 
-        mFusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
+            String start = Double.toString(ClientLocation.latitude) + ',' + Double.toString(ClientLocation.longitude);
+            String end = start;
 
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+            if(StartAddress != null)
+                start = StartAddress;
+            if(EndAddress != null)
+                end=EndAddress;
+
+            DirectionsApiRequest req = DirectionsApi.getDirections(context, start, end);
+            req.optimizeWaypoints(true);
+            req.waypoints(RouteAddresses);
+
+            DirectionsResult res = req.await();
+
+            if (res.routes != null && res.routes.length > 0) {
+                DirectionsRoute route = res.routes[0];
+
+                if (route.legs !=null) {
+                    markers.add(new Pair<>(new LatLng(route.legs[0].startLocation.lat, route.legs[0].startLocation.lng), route.legs[0].startAddress));
+
+                    for(int i=0; i<route.legs.length; i++) {
+                        DirectionsLeg leg = route.legs[i];
+                        markers.add(new Pair<>(new LatLng(route.legs[i].endLocation.lat, route.legs[i].endLocation.lng), route.legs[i].endAddress));
+                        JourneyTime += route.legs[i].duration.inSeconds;
+                        JourneyLength += route.legs[i].distance.inMeters;
+
+                        if (leg.steps != null) {
+                            for (int j=0; j<leg.steps.length;j++){
+                                DirectionsStep step = leg.steps[j];
+                                if (step.steps != null && step.steps.length >0) {
+                                    for (int k=0; k<step.steps.length;k++){
+                                        DirectionsStep step1 = step.steps[k];
+                                        EncodedPolyline points1 = step1.polyline;
+                                        if (points1 != null) {
+                                            //Decode polyline and add points to list of route coordinates
+                                            List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
+                                            for (com.google.maps.model.LatLng coord1 : coords1) {
+                                                path.add(new LatLng(coord1.lat, coord1.lng));
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    EncodedPolyline points = step.polyline;
+                                    if (points != null) {
+                                        //Decode polyline and add points to list of route coordinates
+                                        List<com.google.maps.model.LatLng> coords = points.decodePath();
+                                        for (com.google.maps.model.LatLng coord : coords) {
+                                            path.add(new LatLng(coord.lat, coord.lng));
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
-                });
+                }
+            }
+        } catch(Exception ex) {
+            Toast.makeText(this, ex.toString(), Toast.LENGTH_LONG).show();
+        }
+        if (path.size() > 0) {
+            if(EndAddress==null && StartAddress==null) markers.remove(markers.size()-1);
+            for (int i=0;i<markers.size();i++) {
+                LatLng l = markers.get(i).first;
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(l);
+                markerOptions.title("Pozycja w trasie : " +  Integer.toString(i));
+                markerOptions.snippet(markers.get(i).second);
 
+                mMap.addMarker(markerOptions);
+            }
+            PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLUE).width(5);
+            mMap.addPolyline(opts);
+
+            //Toast for journey time and length
+            Toast.makeText(this,"Estimated time : " + FormatTimeFromSeconds(JourneyTime) + "\nDistance : " + FormatDistanceFromMeters(JourneyLength) , Toast.LENGTH_LONG).show();
+        }
     }
 
     public void onClientListClick(View view){
         Intent intent = new Intent(this, ClientListActivity.class);
         startActivity(intent);
     }
+    private static String FormatTimeFromSeconds(long longVal)
+    {
+        int hours = (int) longVal / 3600;
+        int remainder = (int) longVal - hours * 3600;
+        int mins = remainder / 60;
+        remainder = remainder - mins * 60;
+        int secs = remainder;
+        return  String.format("%02d:%02d:%02d", hours, mins, secs);
+    }
+    private static String FormatDistanceFromMeters(int meters)
+    {
+        int km = meters/1000;
+        meters = meters - km*1000;
+        return km+","+meters + " km";
+    }
+    private final LocationListener locationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            ClientLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 }
---
+++ app/src/main/java/com/example/szafa/myway/OptionsActivity.java
@@ -0,0 +1,21 @@
+package com.example.szafa.myway;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class OptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
